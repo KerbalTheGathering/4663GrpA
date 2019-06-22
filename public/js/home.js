27a816ccfb604ff5f757b522a69f18bfb0a8dd40
Garrett Bloome <garrettbloome@gmail.com>
2019-06-22 17:17:12 -0400

    Account page updated to fit with index and homepage

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,7 @@ if("serviceWorker" in navigator)
     navigator.serviceWorker.register("../pages/home_serviceworker.js")
     .then((registration) =>
     {
-        console.log("home_serviceworker registered with scope: ", registration.scope);
+        console.log("Homepage serviceworker registered with scope: ", registration.scope);
     }).catch((err) =>
     {
         console.log("Service worker registration failed: ", err);
