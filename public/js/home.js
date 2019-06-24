e127f3ff13ee798a31caa30c979d5c6a8e27a4dc
Garrett Bloome <garrettbloome@gmail.com>
2019-06-24 00:23:06 -0400

    Template for homepage carousel articles complete.

--- public/js/home.js
+++ public/js/home.js
@@ -10,3 +10,21 @@ if("serviceWorker" in navigator)
     });
 }
     
+(($) =>
+{
+    $(document).ready(() =>
+    {
+        var carouselTemplate = $('#hidden-template').html();
+
+        for (var i = 0; i < 3; i++)
+        {
+            var item = $(carouselTemplate).clone();
+            $(item).find('#carousel-headline').html("Article headline #" + i);
+            $(item).find('#carousel-hook').html("This is some catchy description that pulls you into the story.");
+            $(item).find('#carousel-button-text').html("Read");
+            
+            $("#carousel_" + i).append(item);
+            console.log("appended carousel_" + i);
+        }
+    });
+})(jQuery);
